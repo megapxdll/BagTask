be4d178d8a2c2cbb77fdc2150fa1bd9149142e3d
megapxdll <mkuanyshbek97@mail.ru>
2021-12-27 12:25:34 +0600

    Pow method

--- src/main.java
+++ src/main.java
@@ -4,6 +4,11 @@ public class main {
 
     public static void main(String[] args) {
 
+        double someValue = 5;
+        double degreeValue = 3;
+        System.out.println(PowMethod(someValue));
+        System.out.println(PowMethod2(someValue, degreeValue));
+
         Items[] items = new Items[] {new Items(3, 150, "banana"), new Items(1, 250000, "ring"),
                 new Items(5, 500000, "notebook"), new Items(2, 10000, "powerbank"),
                 new Items(3, 350000, "iphone")};
@@ -12,6 +17,14 @@ public class main {
 
         bag.setMaxCost(findBestRes(items, items.length-1, bag.getMaxMass()));
         System.out.println(bag.getMaxCost());
+    }
+
+    private static double PowMethod(double someValue) { // Pow without degree parameter using Math library
+        return someValue * someValue;
+    }
+
+    private static double PowMethod2(double someValue, double degreeValue) {
+        return Math.pow(someValue, degreeValue);
     }
 
     private static int findBestRes(Items[] items, int i, int mass) {
